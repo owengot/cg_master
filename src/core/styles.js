774b9103b202c2c2aaafb86ea7d913973d67add2
owengot <owengot@gmail.com>
2023-07-08 13:38:07 +0100

    Initial commit

---
+++ src/core/styles.js
@@ -0,0 +1,11 @@
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/vue/css/core.css';
+
+import '../styles/pages.css';
+
+/* Theme variables */
+import '../theme/variables.css';
+
+import '../assets/bootstrap-grid.min.css';
+
+import 'uno.css';
